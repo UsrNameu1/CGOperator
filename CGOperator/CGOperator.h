d3c75357e16132249b36a40621d4169df39f2cdf
adachi.yuichi <adachi.yuichi@classmethod.jp>
2015-01-30 21:21:30 +0900

    Initial Commit

---
+++ CGOperator/CGOperator.h
@@ -0,0 +1,19 @@
+//
+//  CGOperator.h
+//  CGOperator
+//
+//  Created by adachi.yuichi on 2015/01/30.
+//  Copyright (c) 2015年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGOperator.
+FOUNDATION_EXPORT double CGOperatorVersionNumber;
+
+//! Project version string for CGOperator.
+FOUNDATION_EXPORT const unsigned char CGOperatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGOperator/PublicHeader.h>
+
+
